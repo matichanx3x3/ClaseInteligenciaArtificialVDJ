53eb9afa2f36ac729743da89281be6bcf45734ee
Alejandro Maticorena <amqgamedev@gmail.com>
2026-02-16 16:48:43 +0100

    a

---
+++ Assets/IA2025/Ants/HormiguitasOrdenando_Plantilla.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HormiguitasOrdenando_Plantilla : MonoBehaviour
+{
+    public class Ant
+    {
+        public int x;
+        public int y;
+        int xRange;
+        int yRange;
+        byte[,] map;
+        byte charge;
+        public Ant(int rx, int ry,byte[,] theMap)
+        {
+            x = Random.Range(0, rx);
+            y = Random.Range(0, ry);
+            xRange =rx;
+            yRange = ry;
+            map = theMap;
+            charge = 0;
+        }
+        //CON 7 Y 4 FUNCIONA
+        public void StepAnt()
+        {
+            if(charge == 0)
+            {
+                charge = map[y,x];
+                map[y,x] = 0;
+            }
+            else
+            {
+                if (CountElements )
+                {
+                    
+                }
+                map[y,x] = charge;
+            }
+            
+            int rdm= Random.Range(0,4);
+
+            switch (rdm)
+            {
+                case 0:
+                    if(x < xRange-1)
+                        x += 1;
+                    else
+                        x = xRange-2; 
+                break;
+                case 1:
+                    if(y < yRange-1)
+                        y+=1;
+                    else
+                        y = yRange-2; 
+                break;
+                case 2:
+                    if(x > 0)
+                        x -= 1;
+                    else
+                        x = 1; 
+                break;
+                case 3:
+                    if(y > 0)
+                        y -= 1;
+                    else
+                        y = 1;
+                break;
+            } 
+            
+           //recoge con un radio de 7 y deja con un radio de 4.
+            //si no tiene nada tiene que coger algo.
+            /**/
+
+            //cuando tiene que coger algo?
+            //si hay muy pocas cosas de este tipo a su alrededor.
+            //es por probabilidad. -> si a mi alrededor hay 25 cuadrado y hay una sola azul, se la lleva.
+            //entonces cuando tiene una caja azul y de forma aleatoria llegara a un 
+            
+        }
+
+
+        int CountElements(int type, int px,int py,int ks) //px y px -> punto donde esta la hormiga. ks (rango del radio que la hormiga puede ver).
+        {
+            int equal = 0;
+            for (int j = py; j < ks; j++)
+            {
+                for (int i = px; i < ks; i++)
+                {
+                    
+                }
+            }
+            // contar elementos cerca
+            //bucle de las posiciones de la hormiga la pos y -ks hasta la pos y - ks.
+            //cuantas casillas son del tipo -> =++; (usar bucles anidados)
+            return equal;
+        }
+    }
+
+    // Start is called before the first frame update
+    public byte[,] grid;
+    public int heigh=40;
+    public int width=40;
+    public int antsNumber=64;
+    public bool simulate = true;
+
+    public Ant[] antsList;
+    void Start()
+    {
+        grid = new byte[heigh, width];
+
+        for (int y = 0; y < heigh; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                grid[y, x] = Random.Range(0.0f, 1.0f) > 0.38f ? (byte)0 : (byte)Random.Range(1,5);
+            }
+        }
+        antsList = new Ant[antsNumber];
+        for(int a = 0; a < antsNumber; a++)
+        {
+            antsList[a] = new Ant(width,heigh,grid);
+        }
+    }
+
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        for (int a = 0; a < antsNumber; a++)
+        {
+            antsList[a].StepAnt();
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (grid != null) { 
+        Color actualColor = Color.white;
+        Color nextColor = Color.white;
+        Color clean = Color.white;
+        Color cookie= new Color(0.6f,0.4f,0.4f);
+        Color water = new Color(0.3f, 0.5f, 0.9f);
+        Color meat = new Color(0.9f, 0.2f, 0.2f);
+        Color leaf = new Color(0.3f, 0.8f, 0.3f);
+        for (int y=0;y<heigh;y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[y, x]==0)
+                {
+                    nextColor = clean;
+                }else if (grid[y, x] == 1)
+                {
+                    nextColor = cookie;
+                }
+                else if (grid[y, x] == 2)
+                {
+                    nextColor = water;
+                }
+                else if (grid[y, x] == 3)
+                {
+                    nextColor = meat;
+                }
+                else if (grid[y, x] == 4)
+                {
+                    nextColor = leaf;
+                }
+                if (nextColor != actualColor)
+                {
+                    actualColor = nextColor;
+                    Gizmos.color = actualColor;
+                    
+                }
+                Gizmos.DrawCube(new Vector3(x,y,0),Vector3.one);
+            }
+        }
+
+            Gizmos.color = new Color(0.0f, 0.0f, 0.0f, 0.66f);
+            for (int a = 0; a < antsNumber; a++)
+            {
+                Gizmos.DrawCube(new Vector3(antsList[a].x, antsList[a].y,-1), Vector3.one); 
+            }
+
+        }
+    }
+}
---
+++ Assets/IA2025/MiniAgents/CubeInstancing.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInstancing : MonoBehaviour
+{
+    public Material[] materials= new Material[0];
+    public int width  = 64;
+    public int height = 64;
+    int[,] table;
+    public Mesh meshCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+        Camera.main.transform.position = Vector3.right * ((width / 2)-0.5f) + (Vector3.up * ((height / 2)-0.5f)) - Vector3.forward*(width>height?width:height)*0.5f;
+    }
+    void BoxesLogic()
+    {
+        //For each cell we are going to apply a set of rules.
+        for (int y = 1; y < height-1; y++)
+        {
+            for (int x = 1; x < width-1; x++)
+            {
+                //0 Empty
+                //1 Border(can not be removed)
+                //2 Wall can be removed
+                //3 Sand it has a set of rules
+                //4 Water it has a set of rules
+
+                if (table[y, x] == 3)//If the cell is sand...
+                {
+                    //If there is an empty hole at the bottom just move this square 1 position down
+                    if (table[y-1,x] == 0)
+                    {
+                        table[y, x] = 0;
+                        table[y - 1, x] = 3;
+                    }
+                    else if (table[y - 1, x +1]== 0 && table[y,x+1] == 0)
+                    {
+                        table[y, x] = 0;
+                        table[y - 1, x + 1] = 3;
+                    }
+                    else if (table[y - 1, x - 1] == 0 && table[y, x - 1] == 0)
+                    {
+                        table[y, x] = 0;
+                        table[y - 1, x - 1] = 3;
+                    }
+
+                }
+
+                //If there is not and empty hole at the bottom 
+                //Take a look af the boto right. If the bottom right is empty
+                //then move this square to that position.
+
+                //Same for the left
+
+
+                //If there is an empty hole at the bottom them like the sand the water
+                //goes 1 square down.
+
+                //if there is an empty hole at the right or left choose any
+                //side randomly and move the square there
+                
+
+            }
+        }
+
+
+    }
+    private void FixedUpdate()
+    {
+        BoxesLogic();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r =Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(r.origin, r.direction);
+        Vector3 intersection;
+        LinePlaneIntersection(out intersection, r.origin, r.direction, -Vector3.forward, Vector3.zero);
+        int x = Mathf.RoundToInt(intersection.x);
+        int y= Mathf.RoundToInt(intersection.y);
+        try { 
+        if (table[y, x] != 1)
+        {
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                //Clean white
+                table[y, x] = 0;
+            }
+            else if (Input.GetKey(KeyCode.Alpha2))
+            {
+                //Clean white
+                table[y, x] = 2;
+            }
+            else if (Input.GetKey(KeyCode.Alpha3))
+            {
+                //Clean white
+                table[y, x] = 3;
+            }
+            else if (Input.GetKey(KeyCode.Alpha4))
+            {
+                //Clean white
+                table[y, x] = 4;
+            }
+            else if (Input.GetKey(KeyCode.Alpha5))
+            {
+                //Clean white
+                //table[y, x] = 5;
+            }
+        }
+        }catch
+        {
+
+        }
+        BoxesLogic();
+        DrawCubes();
+    }
+
+
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        width = width >= 256 ? 256 : width;
+        width = width <= 8 ? 8: width;
+        height = height >= 256 ? 256 : height;
+        height = height <= 8 ? 8 : height;
+
+        table = new int[height, width];
+
+        for(int c = 0; c < width;c++)
+        {
+            table[0, c] = 1;
+            table[height-1, c] = 1;
+        }
+        for (int c = 0; c < height; c++)
+        {
+            table[c, 0] = 1;
+            table[c, width-1] = 1;
+        }
+
+    }
+
+    public void DrawCubes()
+    {
+        if (table != null)
+        {
+            List<Matrix4x4>[] matrixBatches = new List<Matrix4x4>[materials.Length];
+            for (int c = 0; c < matrixBatches.Length; c++)
+            {
+                matrixBatches[c] = new List<Matrix4x4>();
+
+            }
+
+            for (int y = 0; y < width; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    matrixBatches[table[y, x]].Add(Matrix4x4.TRS(Vector3.up * y + Vector3.right * x, Quaternion.identity, Vector3.one));
+                }
+            }
+
+            for (int c = 0; c < matrixBatches.Length; c++)
+            {
+                int start = 0;
+                int end = matrixBatches[c].Count;
+                while (start!=end)
+                {
+                    int currentLength = end - start > 1023?1023:end-start;
+                    List<Matrix4x4> miniBatch = matrixBatches[c].GetRange(start, currentLength);
+                    Graphics.DrawMeshInstanced(meshCube, 0, materials[c], miniBatch);
+                    start += currentLength;
+                }
+                
+            }
+        }
+
+    }
+
+    public static bool LinePlaneIntersection(out Vector3 intersection, Vector3 linePoint, Vector3 lineVec, Vector3 planeNormal, Vector3 planePoint)
+    {
+
+        float length;
+        float dotNumerator;
+        float dotDenominator;
+        Vector3 vector;
+        intersection = Vector3.zero;
+
+        //calculate the distance between the linePoint and the line-plane intersection point
+        dotNumerator = Vector3.Dot((planePoint - linePoint), planeNormal);
+        dotDenominator = Vector3.Dot(lineVec, planeNormal);
+
+        //line and plane are not parallel
+        if (dotDenominator != 0.0f)
+        {
+            length = dotNumerator / dotDenominator;
+
+            //create a vector from the linePoint to the intersection point
+            vector = lineVec.normalized * length;
+            //vector = SetVectorLength(lineVec, length);
+
+            //get the coordinates of the line-plane intersection point
+            intersection = linePoint + vector;
+
+            return true;
+        }
+
+        //output not valid
+        else
+        {
+            return false;
+        }
+    }
+}
